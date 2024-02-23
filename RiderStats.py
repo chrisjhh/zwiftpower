e2a254c97f6e6f55f0e69fb05e9665af6c65746d
Chris Hamer-Hodges <chris.hhodges@gmail.com>
2024-02-23 17:36:04 +0000

    Add class for individual race results

--- RiderStats.py
+++ RiderStats.py
@@ -1,9 +1,63 @@
-from .rider import RiderStatsRequest, JSONRiderStats
+from datetime import datetime
+from .rider import RiderStatsRequest, JSONRiderStats, JSONRaceResult
 
-def getRiderStats():
-    rs = RiderStatsRequest()
+def getRiderStats(timestamp=None):
+    rs = RiderStatsRequest(timestamp=timestamp)
     data = rs.get()
     return RiderStats(data)
+
+
+class RaceResult:
+
+    def __init__(self, data: JSONRaceResult) -> None:
+        self.data = data
+
+    @property
+    def name(self):
+        return self.data["event_title"]
+    
+    @property
+    def timestamp(self):
+        return self.data["event_date"]
+    
+    @property
+    def date(self):
+        return datetime.fromtimestamp(self.timestamp)
+    
+    @property
+    def category(self):
+        return self.data["category"]
+    
+    @property
+    def zid(self):
+        return int(self.data["zid"])
+    
+    @property
+    def position(self):
+        return self.data["position_in_cat"]
+    
+    @property
+    def overallPosition(self):
+        return self.data["pos"]
+    
+    @property
+    def score(self):
+        return float(self.data["skill"])
+    
+    @property
+    def ratingBefore(self):
+        return float(self.data["skill_b"])
+    
+    @property
+    def skillGain(self):
+        return float(self.data["skill_gain"])
+    
+    @property
+    def ratingAfter(self):
+        before = self.ratingBefore
+        if before == 0:
+            before = 600
+        return before - self.skillGain
 
 class RiderStats:
 
@@ -14,23 +68,28 @@ class RiderStats:
         res = [x for x in self.data if "TYPE_RACE" in x["f_t"]]
         res.sort(key = lambda x : x["event_date"])
         res.reverse()
-        return res
+        return [RaceResult(r) for r in res]
+    
+    def findRide(self, timestamp: int):
+        for r in self.data:
+            ts = r["event_date"]
+            if abs(timestamp - ts) < 200:
+                return RaceResult(r)
+        return None
+
     
 if __name__ == "__main__":
-    from datetime import datetime
     from .RaceStats import getRaceStats
     stats = getRiderStats()
     res = stats.races()
     for r in res:
-        dt = datetime.fromtimestamp(r["event_date"])
+        dt = r.date
         date = dt.strftime("%d/%m/%y")
-        before = float(r["skill_b"])
-        if before == 0:
-            before = 600
-        zid = r["zid"]
-        cat = r["category"]
+        before = r.ratingBefore
+        zid = r.zid
+        cat = r.category
         rstats = getRaceStats(zid)
         num = rstats.numberOfRiders(cat)
-        pos = "{}/{}".format(r["position_in_cat"], num)
-        print("{} {} ({}) {} {} {} -> {:.2f}".format(date, r["event_title"], cat, r["skill"], pos, zid, before - float(r["skill_gain"])))
+        pos = "{}/{}".format(r.position, num)
+        print("{} {} {} ({}) {} {} {} -> {:.2f}".format(date, r.timestamp,r.name, cat, r.score, pos, zid, r.ratingAfter))
 
